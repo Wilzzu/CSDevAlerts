9d8831de2cc8d9e81ed4effc4c5a1c9fa3c80fd6
Wilzzu <11214356+Wilzzu@users.noreply.github.com>
2022-08-17 17:51:26 +0300

    Made the bot public

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const SteamUser = require("steam-user");
 const CronJob = require("cron").CronJob;
 const config = require("./config.json");
+let servers = require("./servers.json");
 const valveids = require("./allids.json");
+//ACTUALLY DEVS, NOT RANDOMS
 const randomids = require("./randoms.json");
+const fs = require("fs");
 var client = new SteamUser();
 
 const { Client, GatewayIntentBits, EmbedBuilder } = require("discord.js");
@@ -10,6 +13,7 @@ const bot = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 bot.once("ready", () => {
 	console.log("Discord bot ready!");
+	bot.user.setPresence({ activities: [{ name: "/help for all commands" }] });
 });
 
 if (config.twofa) {
@@ -72,11 +76,25 @@ const getRichPresence = (appID, iteration) => {
 							map: response.users[key].richPresence["game:map"],
 							server: response.users[key].richPresence["game:server"],
 						};
-						console.log("FOUND");
 
-						if (randomids.all.includes(user.id) && appID[iteration] == 730) {
-							console.log("Found user but they aren't in dev branch");
-						} else foundUsers.push(user);
+						//DEV DEV
+						if (appID[iteration] == 710) {
+							user.dev = "CS DEV";
+							user.who = "devBranch";
+							foundUsers.push(user);
+						}
+						//DEV ALL
+						else if (randomids.all.includes(user.id)) {
+							user.dev = "CS DEV";
+							user.who = "devAll";
+							foundUsers.push(user);
+						}
+						//VALVE ALL
+						else {
+							user.dev = "VALVE EMPLOYEE";
+							user.who = "all";
+							foundUsers.push(user);
+						}
 
 						if (key == Object.keys(response.users)[Object.keys(response.users).length - 1]) {
 							getRichPresence(appID, iteration + 1);
@@ -125,8 +143,27 @@ const sendAlert = async (info, mapChanged) => {
 		let alertEmbed = await createEmbed(e, mapChanged);
 		try {
 			bot.guilds.cache.forEach((guild) => {
-				let channel = guild.channels.cache.find((e) => e.type === 0); // Send to first text channel available
-				channel.send({ embeds: [alertEmbed] });
+				// Find settings
+				let settings = servers.find((setting) => setting.id == guild.id);
+				if (settings.stop) return;
+				if (
+					(settings.who == "devBranch" && e.who == "devBranch") ||
+					(settings.who == "devAll" && (e.who == "devAll" || e.who == "devBranch")) ||
+					settings.who == "all"
+				) {
+					if (settings.channel == "No channel selected") {
+						let channel = guild.channels.cache.find((ch) => ch.type === 0); // Send to first text channel available
+						channel.send({ embeds: [alertEmbed] });
+					} else {
+						if (guild.id == "924300455151034429") {
+							bot.channels.cache
+								.get(settings.channel)
+								.send({ content: e.id, embeds: [alertEmbed] });
+						} else {
+							bot.channels.cache.get(settings.channel).send({ embeds: [alertEmbed] });
+						}
+					}
+				}
 			});
 		} catch (err) {
 			console.log("Could not send message to guild");
@@ -139,13 +176,18 @@ const sendAlert = async (info, mapChanged) => {
 const createEmbed = async (info, mapChanged) => {
 	console.log(info);
 	let title = "SOMETHING NEW HAPPENED!";
-	if (mapChanged) title = "CS DEV CHANGED MAP!";
-	else title = "CS DEV STARTED PLAYING!";
-	desc = `***General info:***\n👨‍🔬 **ID:** ||${info.id}||\n🕹 **Game:** ${info.game}\nℹ **Info:** ${
-		info.status
-	}\n⏺ **Status:** ${info.statusLocalized}\n⭕ **State**: ${
-		info.state.charAt(0).toUpperCase() + info.state.slice(1)
-	}\n\n***In-depth info:***`;
+	if (mapChanged) title = info.dev + " CHANGED MAP/MAPGROUP!";
+	else title = info.dev + " STARTED PLAYING!";
+	let desc = `***General info:***\n`;
+	//${info.id}
+	if (info.id) desc = desc + `👨‍🔬 **ID:** ||⬛⬛⬛⬛⬛⬛⬛⬛||\n`;
+	if (info.game) desc = desc + `🕹 **Game:** ${info.game}\n`;
+	if (info.status) desc = desc + `ℹ **Info:** ${info.status}\n`;
+	if (info.statusLocalized) desc = desc + `⏺ **Status:** ${info.statusLocalized}\n`;
+	if (info.state)
+		desc = desc + `⭕ **State**: ${info.state.charAt(0).toUpperCase() + info.state.slice(1)}\n`;
+	desc = desc + `\n***In-depth info:***`;
+
 	let alertEmbed = new EmbedBuilder()
 		.setTitle(title)
 		.setDescription(desc)
@@ -196,6 +238,88 @@ const createEmbed = async (info, mapChanged) => {
 client.on("error", function (e) {
 	// Some error occurred during logon
 	console.log(e);
+});
+
+bot.on("interactionCreate", async (interaction) => {
+	if (!interaction.isChatInputCommand()) return;
+	const { commandName } = interaction;
+
+	if (commandName === "status") {
+		await interaction.reply("CSDevAlerts is up and running!");
+	}
+	//
+	else if (commandName === "here") {
+		let index = servers.findIndex((e) => e.id == interaction.guildId);
+		servers[index].channel = interaction.channelId;
+		fs.writeFileSync("./servers.json", JSON.stringify(servers, null, 2));
+		await interaction.reply("Alert channel set! You will now receive alerts here!");
+	}
+	//
+	else if (commandName === "stop") {
+		let index = servers.findIndex((e) => e.id == interaction.guildId);
+		servers[index].stop = true;
+		fs.writeFileSync("./servers.json", JSON.stringify(servers, null, 2));
+		await interaction.reply("You wont receive alerts anymore!");
+	}
+	//
+	else if (commandName === "continue") {
+		let index = servers.findIndex((e) => e.id == interaction.guildId);
+		servers[index].stop = false;
+		fs.writeFileSync("./servers.json", JSON.stringify(servers, null, 2));
+		await interaction.reply("You will receive alerts again!");
+	}
+	//
+	else if (commandName === "help") {
+		let helpEmbed = new EmbedBuilder()
+			.setTitle("Commands for CSDevAlerts!")
+			.setDescription(
+				"📨 **/here** - Type this command in the channel you want to receive alerts at!\n👨‍🔬 **/who** - Select who should trigger an alert!\n❌ **/stop** - Stop receiving alerts!\n✅ **/continue** - Continue receiving alerts again!\n🖥 **/status** - Check if CSDevAlerts is running!\nℹ **/help** - Show all commands for CSDevAlerts!"
+			)
+			.setThumbnail("https://i.imgur.com/tVO9yYh.png");
+		await interaction.reply({ embeds: [helpEmbed] });
+	}
+	//
+	else if (commandName === "who") {
+		let choice = interaction.options._hoistedOptions[0].value;
+		let res = "";
+		if (choice == "who_devAll") {
+			let index = servers.findIndex((e) => e.id == interaction.guildId);
+			servers[index].who = "devAll";
+			fs.writeFileSync("./servers.json", JSON.stringify(servers, null, 2));
+			res = "from CS devs on public and dev branches [730 + 710]!";
+		}
+		if (choice == "who_devBranch") {
+			let index = servers.findIndex((e) => e.id == interaction.guildId);
+			servers[index].who = "devBranch";
+			fs.writeFileSync("./servers.json", JSON.stringify(servers, null, 2));
+			res = "only from dev branch [710]!";
+		}
+		if (choice == "who_all") {
+			let index = servers.findIndex((e) => e.id == interaction.guildId);
+			servers[index].who = "all";
+			fs.writeFileSync("./servers.json", JSON.stringify(servers, null, 2));
+			res = "from all Valve employees on any branch!";
+		}
+		await interaction.reply("You will now receive alerts " + res);
+	}
+});
+
+bot.on("guildCreate", (guild) => {
+	console.log("ADDED TO GUILD");
+	if (servers.find((e) => e.id == guild.id)) return;
+	else {
+		console.log("NEW GUILD");
+		let temp = servers;
+		let addGuild = {
+			id: guild.id,
+			channel: "No channel selected",
+			stop: false,
+			who: "devAll",
+		};
+		temp.push(addGuild);
+		fs.writeFileSync("./servers.json", JSON.stringify(temp, null, 2));
+	}
+	//guild.systemChannel.send(`yo`);
 });
 
 bot.login(config.token);
